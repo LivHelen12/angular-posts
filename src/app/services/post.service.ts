1e2490f65e827bc16ad526428816e4781852b95a
Livia <liviahelen025@gmail.com>
2024-02-22 13:25:28 -0300

    Add createPost method in post services.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,15 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Post } from '../types/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private postsUrl = './assets/data.json';
 
   constructor(private http: HttpClient) { }
 
-  getPosts() {
-    return this.http.get<Post[]>("./assets/data.json");
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl).pipe(
+      catchError(() => {
+        console.error('Erro ao obter os posts do servidor. Retornando array vazio.');
+        return of([]);
+      })
+    );
+  }
+
+  createPost(content: string): Observable<Post[]> {
+    return this.getPosts().pipe(
+      map(posts => {
+        const newPost: Post = {
+          id: Math.floor(Math.random() * 1000),
+          name: "Novo usuário",
+          username: "novo_usuario",
+          photo_url: "https://i.pravatar.cc/150?u=novo_usuario",
+          postedAt: new Date(),
+          content: content
+        };
+        posts.push(newPost);
+        return posts;
+      })
+    );
   }
 }
