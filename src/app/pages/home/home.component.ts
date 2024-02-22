91f39f1fa079d031f44d8b4f371189f2de4cd2f2
Livia <liviahelen025@gmail.com>
2024-02-22 12:46:42 -0300

    Show all posts at home page.

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,30 @@
+import { PostService } from './../../services/post.service';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormPostComponent } from '../../components/post/form/form.component';
+import { CardComponent } from '../../components/post/card/card.component';
+import { Post } from '../../types/post';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormPostComponent,
+    CardComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  posts!: Observable<Post[]>;
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
+}
---
+++ src/app/types/post.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  id: number;
+  name: string;
+  username: string;
+  postedAt: string;
+  content: string;
+  photo_url: string;
+}
