5dac0c1da1b801e64bfbb4e47f5caa614d3075eb
Livia <liviahelen025@gmail.com>
2024-02-22 12:45:02 -0300

    Set card to receive dynamic posts.

--- src/app/components/post/card/card.component.ts
+++ src/app/components/post/card/card.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PhotoComponent } from '../../user/photo/photo.component';
 import { ContainerComponent } from '../container/container.component';
+import { Post } from '../../../types/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [PhotoComponent, ContainerComponent],
+  imports: [CommonModule, PhotoComponent, ContainerComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
 
+export class CardComponent {
+  @Input() post!: Post;
+
+  constructor() { }
 }
