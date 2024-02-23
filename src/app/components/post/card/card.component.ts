6cff28e46daa16bee7467428318eca997a8428e4
Livia <liviahelen025@gmail.com>
2024-02-23 11:55:04 -0300

    Update card component.

--- src/app/components/post/card/card.component.ts
+++ src/app/components/post/card/card.component.ts
@@ -5,11 +5,17 @@ import { ContainerComponent } from '../container/container.component';
 import { Post } from '../../../types/post';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+import { EmptyComponent } from '../../empty-state/empty-state.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule, PhotoComponent, ContainerComponent],
+  imports: [
+    CommonModule,
+    PhotoComponent,
+    ContainerComponent,
+    EmptyComponent
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
