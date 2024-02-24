af38ab8e68282e8562a6862e9553dfe58224ec60
Livia <liviahelen025@gmail.com>
2024-02-24 15:35:03 -0300

    Use load component instead of componente.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: "",
-    component: HomeComponent,
+    loadComponent() {
+      return import('./pages/home/home.component').then(m => m.HomeComponent);
+    },
   }
 ];
