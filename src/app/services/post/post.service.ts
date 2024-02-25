f6566a7d781822ba989047f146dbab8d0f218c0e
Livia <liviahelen025@gmail.com>
2024-02-24 21:26:42 -0300

    Refactor post service to use behavir subject.

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,4 +1,6 @@
+import { StorageService } from './../storage/storage.service';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Post } from '../../types/post';
 
 @Injectable({
@@ -6,5 +8,36 @@ import { Post } from '../../types/post';
 })
 
 export class PostService {
-  constructor() { }
+  storageKey = "posts";
+  postsSubject$: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
+  post!: Post[];
+
+  constructor(private storageService: StorageService) {
+    this.list();
+  }
+
+  create(content: string) {
+    const newPost: Post = {
+      id: Math.random(),
+      name: "Meu usuário",
+      username: "myuser",
+      postedAt: new Date(),
+      content: content,
+      photo_url: "https://avatars.githubusercontent.com/u/62712621?v=4"
+    };
+
+    const currentPosts = this.postsSubject$.getValue();
+    const updatedPosts = [newPost, ...currentPosts];
+
+    this.storageService.set(this.storageKey, updatedPosts);
+    this.postsSubject$.next(updatedPosts);
+  }
+
+  list() {
+    const storedPosts = this.storageService.get(this.storageKey);
+
+    if (storedPosts) {
+      this.postsSubject$.next(storedPosts);
+    }
+  }
 }
