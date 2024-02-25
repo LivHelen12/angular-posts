d55e1ffccf52f8a4fb41d924255f8d484f313742
Livia <liviahelen025@gmail.com>
2024-02-24 21:55:41 -0300

    Destroy the post subject when the component does not exist anymore.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormPostComponent } from '../../components/post/form/form.component';
 import { CardComponent } from '../../components/post/card/card.component';
@@ -18,7 +18,7 @@ import { PostService } from '../../services/post/post.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   posts!: Post[];
 
   constructor(private postService: PostService) { }
@@ -31,5 +31,9 @@ export class HomeComponent implements OnInit {
     this.postService.postsSubject$.subscribe((posts) => {
       this.posts = posts;
     })
+  }
+
+  ngOnDestroy() {
+    this.postService.postsSubject$.unsubscribe();
   }
 }
