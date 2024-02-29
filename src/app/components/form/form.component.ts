05671ec05cd5bf73143b2d3d96dd9030cace74a4
Livia <liviahelen025@gmail.com>
2024-02-29 15:49:23 -0300

    Add verification to see if a change happened in the field

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -52,4 +52,8 @@ export class FormPostComponent implements OnChanges {
       this.postForm.reset();
     }
   };
+  isTweetButtonDisabled() {
+    const inputField = this.postForm.get("post");
+    return Boolean(inputField?.invalid || inputField?.pristine);
+  }
 }
